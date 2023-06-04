36d829f8f7e71592b39c6f8c662c873671f2f8d5
Evan Jaramillo <41314787+evanjaramillo@users.noreply.github.com>
2023-06-04 17:41:31 -0500

    move viewer into component

---
+++ src/lib/CesiumViewer.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { Viewer } from "cesium";
+    import '../../node_modules/cesium/Build/Cesium/Widgets/widgets.css';
+
+    // make sure to define the cesium assets location:
+    ( window as Window ).CESIUM_BASE_URL = "./cesium";
+
+    let viewer: Viewer;
+
+    onMount( async () => {
+
+        viewer = new Viewer( 'cesiumContainer', {
+            animation: false,
+            baseLayerPicker: false,
+            fullscreenButton: false,
+            vrButton: false,
+            geocoder: false,
+            homeButton: false,
+            sceneModePicker: false,
+            timeline: false,
+            navigationHelpButton: false,
+            navigationInstructionsInitiallyVisible: false,
+            scene3DOnly: true,
+            shouldAnimate: true,
+            creditContainer: 'cesiumCreditContainer'
+        } );
+
+    } );
+</script>
+
+<div id="cesiumContainer">
+</div>
+
+<div id="cesiumCreditContainer">
+</div>
+
+<style>
+    #cesiumContainer {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+    }
+
+    #cesiumCreditContainer {
+        visibility: hidden;
+    }
+</style>
