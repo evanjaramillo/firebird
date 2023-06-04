648bad7af7acd10b3e55135d6ccdc5a164cf1b23
Evan Jaramillo <41314787+evanjaramillo@users.noreply.github.com>
2023-06-04 18:06:19 -0500

    fix top-level .ts files skipping linter

--- vite.config.ts
+++ vite.config.ts
@@ -5,42 +5,42 @@ import sveltePreprocess from "svelte-preprocess";
 
 // https://vitejs.dev/config/
 export default defineConfig( async () => ( {
-	plugins: [
-		// Copy assets required for cesium
-		viteStaticCopy( {
-			targets: [
-				{ src: 'node_modules/cesium/Build/Cesium/Assets', dest: './cesium/' },
-				{ src: 'node_modules/cesium/Build/Cesium/ThirdParty', dest: './cesium/' },
-				{ src: 'node_modules/cesium/Build/Cesium/Widgets', dest: './cesium/' },
-				{ src: 'node_modules/cesium/Build/Cesium/Workers', dest: './cesium/' }
-			]
-		} ),
-		svelte( {
-			preprocess: [
-				sveltePreprocess( {
-					typescript: true,
-				} ),
-			],
-		} )
-	],
+    plugins: [
+        // Copy assets required for cesium
+        viteStaticCopy( {
+            targets: [
+                { src: 'node_modules/cesium/Build/Cesium/Assets', dest: './cesium/' },
+                { src: 'node_modules/cesium/Build/Cesium/ThirdParty', dest: './cesium/' },
+                { src: 'node_modules/cesium/Build/Cesium/Widgets', dest: './cesium/' },
+                { src: 'node_modules/cesium/Build/Cesium/Workers', dest: './cesium/' }
+            ]
+        } ),
+        svelte( {
+            preprocess: [
+                sveltePreprocess( {
+                    typescript: true,
+                } ),
+            ],
+        } )
+    ],
 
-	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-	// prevent vite from obscuring rust errors
-	clearScreen: false,
-	// tauri expects a fixed port, fail if that port is not available
-	server: {
-		port: 1420,
-		strictPort: true,
-	},
-	// to make use of `TAURI_DEBUG` and other env variables
-	// https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-	envPrefix: [ "VITE_", "TAURI_" ],
-	build: {
-		// Tauri supports es2021
-		target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-		// don't minify for debug builds
-		minify: ! process.env.TAURI_DEBUG ? "esbuild" : false,
-		// produce sourcemaps for debug builds
-		sourcemap: !! process.env.TAURI_DEBUG,
-	}
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    // prevent vite from obscuring rust errors
+    clearScreen: false,
+    // tauri expects a fixed port, fail if that port is not available
+    server: {
+        port: 1420,
+        strictPort: true,
+    },
+    // to make use of `TAURI_DEBUG` and other env variables
+    // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+    envPrefix: [ "VITE_", "TAURI_" ],
+    build: {
+        // Tauri supports es2021
+        target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+        // don't minify for debug builds
+        minify: ! process.env.TAURI_DEBUG ? "esbuild" : false,
+        // produce sourcemaps for debug builds
+        sourcemap: !! process.env.TAURI_DEBUG,
+    }
 } ) );
